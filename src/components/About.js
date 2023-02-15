2879f029a8091cb38dc95fb64e43330c998e8e09
deepakkumar <dk44861@gmail.com>
2023-02-15 22:43:25 +0530

    test

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus eaque beatae doloribus harum, modi adipisci iusto tempore perferendis veritatis excepturi quo eos atque laudantium animi iste! Ullam sed rerum saepe.
+    Perferendis totam natus mollitia odit tenetur, earum asperiores tempore cupiditate culpa vero itaque distinctio dolorem in hic corrupti? Nobis quasi in corrupti repellat accusamus corporis! Magni pariatur quod asperiores deleniti!
+    Placeat aliquam ducimus doloremque, possimus eos recusandae dignissimos atque est itaque, illo iusto assumenda optio aperiam tempora, pariatur quasi blanditiis! Iusto eveniet iste sequi ex non sed, laudantium ipsam exercitationem?
+    Facere harum perferendis cumque, eligendi molestias magni, quas numquam amet eius totam et unde quod nihil autem consectetur. Aliquam saepe ullam eos ipsum natus quae voluptate nemo! Mollitia, dolor illo?
+    Aperiam esse consequatur, iste quo repellendus nesciunt accusamus molestias. A ratione neque quisquam odio illo mollitia ab vel adipisci quod rerum cupiditate consectetur, aut, excepturi beatae, earum dicta similique aperiam?
+    Officiis esse eveniet iure porro molestias, cumque omnis ducimus, ipsa animi quis fugiat! Rerum ullam nam consequatur! Rem, minima praesentium consequatur porro eos ipsam, quasi non inventore dolore, aut perferendis.
+    Quisquam voluptatem enim itaque nobis molestiae, quaerat incidunt modi aut hic asperiores, aspernatur assumenda, ex autem ut optio. Tenetur quibusdam suscipit quod! Accusantium iusto hic inventore porro earum est itaque.
+    Nisi, eum consequuntur. Dolor, illo quasi magnam necessitatibus voluptates nam, dolores totam, odit dignissimos obcaecati hic. Aspernatur ducimus neque eius sint quae quidem, optio impedit expedita, cupiditate beatae repellendus dolores?
+    Corrupti consequuntur maxime alias reiciendis, fugiat magnam ut, quia impedit quod ullam fuga nemo suscipit dolorem dignissimos saepe animi maiores nulla earum optio nam tempore quisquam recusandae eligendi quaerat. Harum!
+    In quod est impedit dicta? Itaque, placeat. Earum consectetur quas nam soluta, impedit, ad, placeat magni sequi doloribus itaque eveniet provident culpa molestiae rerum aliquam atque molestias. Libero, sequi reprehenderit?</div>
+  )
+}
---
+++ src/components/Employees/Employees.js
@@ -0,0 +1,26 @@
+import { CircularProgress } from "@mui/material";
+import { Container } from "@mui/system";
+import * as React from 'react';
+import Context from "../ContextFold/Context";
+import Cardbody from "./Cards";
+
+export  function Employees() {
+ 
+  const context = React.useContext(Context);
+
+
+  const style ={
+    display:'flex',
+    flexWrap:'wrap',
+    justifyContent:"center",
+    flexDirection:'row',
+  }
+
+  return (
+    <Container sx={style}>
+      { context.sortedEmployees ==='empty'?<h1>Nothing To Show !</h1> : context.sortedEmployees.length !==0 ? context.sortedEmployees.map((emp,index)=> <Cardbody key={`${index}`} emp={emp}/>):<CircularProgress />}
+    </Container>
+  );
+}
+
+
